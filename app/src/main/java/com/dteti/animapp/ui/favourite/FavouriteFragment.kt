a626ac06d3998109b5403caab73f61fae606e1c1
gladhtput <giladhput@gmail.com>
2021-10-06 10:29:42 +0700

    Add files as first commit
    - [x] Fixing Bottom Navigation
    - [x] Add Icons

---
+++ app/src/main/java/com/dteti/animapp/ui/favourite/FavouriteFragment.kt
@@ -0,0 +1,44 @@
+package com.dteti.animapp.ui.favourite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.dteti.animapp.databinding.FragmentDashboardBinding
+
+class FavouriteFragment : Fragment() {
+
+    private lateinit var favouriteViewModel: FavouriteViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        favouriteViewModel =
+            ViewModelProvider(this).get(FavouriteViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        favouriteViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
