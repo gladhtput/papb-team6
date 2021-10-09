271c453a11e903c5397e1ff59adedcc61218ab13
gladhtput <giladhput@gmail.com>
2021-10-09 10:43:07 +0700

    Fixed about fragment and activity main
    - [x] App can be launched
    - [x] About page done, more edits may come next
    - [x] Remove Header from the app, it might be changed later

--- app/src/main/java/com/dteti/animapp/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/dteti/animapp/ui/about/AboutViewModel.kt
@@ -6,8 +6,5 @@ import androidx.lifecycle.ViewModel
 
 class AboutViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
-    }
-    val text: LiveData<String> = _text
+
 }
